dd7edc5841e2166aa9c4c9ed4970b4fbefb6645d
Arno Hoogma <arno@hoog.ma>
2022-04-05 08:42:33 +0200

    updated publishables, post install command

--- src/ServiceProvider.php
+++ src/ServiceProvider.php
@@ -8,17 +8,21 @@ use Arnohoogma\StatamicImageOptimizer\Fieldtypes\ImageOptimizerFieldtype;
 use Arnohoogma\StatamicImageOptimizer\Commands\ImageOptimizerCommand;
 use Arnohoogma\StatamicImageOptimizer\Listeners\OptimizeAsset;
 use Arnohoogma\StatamicImageOptimizer\Listeners\OptimizeGlide;
+use Illuminate\Support\Facades\Artisan;
 use Statamic\Providers\AddonServiceProvider;
 use Statamic\Events\AssetContainerBlueprintFound;
 use Statamic\Events\GlideImageGenerated;
 use Statamic\Events\AssetUploaded;
 use Statamic\Facades\Utility;
+use Statamic\Statamic;
 
 class ServiceProvider extends AddonServiceProvider
 {
 
+	protected $publishAfterInstall = false;
+
     protected $scripts = [
-      
+
         __DIR__ . '/../public/js/app.js'
 
     ];
@@ -32,43 +36,68 @@ class ServiceProvider extends AddonServiceProvider
     ];
 
     protected $commands = [
-    
+
         ImageOptimizerCommand::class
 
     ];
 
     protected $fieldtypes = [
-        
+
         ImageOptimizerFieldtype::class
 
     ];
 
     public function boot()
     {
-        
+
         parent::boot();
 
+		$this->mergeConfigFrom(__DIR__ . '/../config/config.php', 'statamic.imageoptimizer');
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'imageoptimizer');
         $this->loadViewsFrom(__DIR__ . '/../resources/views/', 'imageoptimizer');
 
-        if ($this->app->runningInConsole())
-        {
-            
-            $this->publishes([__DIR__ . '/../resources/lang' => resource_path('lang/vendor/arnohoogma/statamic-image-optimizer/')], 'lang');
-            $this->publishes([__DIR__ . '/../config/config.php' => config_path('statamic/imageoptimizer.php')], 'config');
+        if ($this->app->runningInConsole()) {
+
+            $this->publishes([
+				__DIR__ . '/../resources/lang' => resource_path('lang/vendor/imageoptimizer/')
+			], 'imageoptimizer-lang');
+
+            $this->publishes([
+				__DIR__ . '/../config/config.php' => config_path('statamic/imageoptimizer.php')
+			], 'imageoptimizer-config');
 
         }
 
-        $utility = Utility::make('ImageOptimizer')->title('ImageOptimizer')->navTitle('Optimizer')->description( __('imageoptimizer::cp.description') )->icon('assets');
-        
+		$this->createUtility();
+		$this->publishAssets();
+
+    }
+
+	private function createUtility()
+	{
+
+		$utility = Utility::make('ImageOptimizer')->title('ImageOptimizer')->navTitle('Optimizer')->description( __('imageoptimizer::cp.description') )->icon('assets');
+
         $utility->routes(function($router) {
-            
+
             $router->get('/', [ImageOptimizerController::class, 'index'])->name('index');
             $router->post('/{encoded_asset}', [ImageOptimizerController::class, 'optimize'])->name('optimize');
 
         });
 
         $utility->register();
+
+	}
+
+	private function publishAssets()
+    {
+
+		Statamic::afterInstalled(function($command) {
+
+            Artisan::call('vendor:publish --tag=imageoptimizer-config');
+            Artisan::call('vendor:publish --tag=imageoptimizer-lang');
+
+        });
 
     }
 
