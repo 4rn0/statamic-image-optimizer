976048e23081aea7c330cb36e619d97dd7d57d98
Arno Hoogma <arno@hoog.ma>
2022-04-07 14:42:55 +0200

    french translation

---
+++ resources/lang/fr/cp.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'title' => 'ImageOptimizer',
+    'description' => 'Optimiser toutes vos images existantes et videz le cache de Glide',
+    'configuration' => 'Configuration',
+    'configuration_path' => 'Les paramètres sont configurés dans <code>:path</code>',
+    'asset' => 'Optimiser les images',
+    'glide' => 'Optimiser Glide',
+    'log' => 'Consigner les optimisations',
+    'optimizers' => 'Optimisateurs',
+    'documentation' => "Veuillez vous référer à la <a href=':url' target='_blank'>documentation</a> pour l'installation et la configuration des optimisateurs",
+    'executable' => 'exécutable',
+    'arguments' => 'paramètres',
+    'mimetype' => 'mimetype',
+    'optimizer_no' => 'Non trouvé. Cette optimisation ne sera pas exécutée',
+    'optimizer_maybe' => 'Non trouvé. La version incluse sera utilisée : :path',
+    'optimizer_yes' => "Trouvé à l'endroit suivant : :path",
+
+    'original' => "La taille d'origine était",
+    'reduced' => 'Réduit de',
+    'images' => 'Images',
+    'optimized' => 'Optimisé',
+    'not-optimized' => "Cette image n'a pas encore été optimisée",
+    'empty' => "Rien n'a encore été optimisé...",
+    'optimizing' => 'Optimisation',
+    'of' => 'sur',
+
+    'optimize' => 'Optimiser',
+    'optimize-new' => 'Optimiser nouveau',
+    'optimize-again' => 'Optimiser à nouveau',
+    
+];
