12a0f100422460b7136b7f4fde7c007ab9ab5e21
Sinan Eldem <sineld@gmail.com>
2022-11-03 03:53:45 +0300

    Turkish language files added

---
+++ resources/lang/tr/cp.php
@@ -0,0 +1,34 @@
+<?php
+
+return [
+
+    'title' => 'Görsel Optimizasyonu',
+    'description' => 'Mevcut tüm görsel varlıklarınızı optimize edin ve Glide önbelleğini temizleyin.',
+    'configuration' => 'Yapılandırma',
+    'configuration_path' => 'Ayarlar <code>:path</code> içinde yapılandırıldı.',
+    'asset' => 'Varlıkları Optimize Et',
+    'glide' => 'Glide\'o Optimize et',
+    'log' => 'Optimizasyon çıktısını günlüğe kaydet',
+    'optimizers' => 'Optimize ediciler',
+    'documentation' => 'Optimize edicilerin nasıl kurulacağı ve yapılandırılacağı ile ilgili lütfen <a href=":url" target="_blank">belgelere</a> bakın.',
+    'executable' => 'Çalıştırılabilir',
+    'arguments' => 'Argümanlar',
+    'mimetype' => 'Mime Türü',
+    'optimizer_no' => 'Bulunamadı. Bu optimizasyon yürütülmeyecek',
+    'optimizer_maybe' => 'Bulunamadı. Buradaki :path dahil edilen sürüm kullanılacaktır.',
+    'optimizer_yes' => 'Şu yolda bulundu: :path',
+
+    'original' => 'İlk boyut şöyleydi:',
+    'reduced' => 'Sıkıştırma aracı',
+    'images' => 'görsel varlık',
+    'optimized' => 'Optimize edilen',
+    'not-optimized' => 'Bu görsel henüz optimize edilmedi',
+    'empty' => 'Henüz optimize edilmiş bir şey yok...',
+    'optimizing' => 'Optimizasyon yapılıyor...',
+    'of' => '#',
+
+    'optimize' => 'Optimize et',
+    'optimize-new' => 'Yeni Optimizasyon',
+    'optimize-again' => 'Tekrar optimize et',
+
+];
